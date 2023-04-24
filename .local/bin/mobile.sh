73df3b78a911b213484420e99b1672d7a96e2a73
chrispwd <chrispwd@protonmail.com>
2023-04-23 23:28:43 -0600

    add keyboard layout switcher

---
+++ .local/bin/mobile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Toggles colemak-dh and switches capslock to control
+if [ $# -eq 0 ]
+then
+    setxkbmap us -variant colemak_dh && \
+        setxkbmap -option "ctrl:nocaps"
+else
+    setxkbmap -layout us
+fi
