6d5c8e034c2d31c76b269014f6cbcb9da81a8454
chrispwd <chrispwd@protonmail.com>
2023-05-12 16:28:54 -0600

    fix corfu popup for terminal

--- .config/chrispmacs/modules/completion.el
+++ .config/chrispmacs/modules/completion.el
@@ -7,6 +7,7 @@
 (cop-install-package-if-not-already 'marginalia)
 (cop-install-package-if-not-already 'orderless)
 (cop-install-package-if-not-already 'corfu)
+(cop-install-package-if-not-already 'corfu-terminal)
 
 ;; Vertico: Minibuffer output of options
 (require 'vertico)
@@ -31,10 +32,13 @@
       completion-category-overrides '((file (styles basic partial-completion))))
 
 (require 'corfu)
+(require 'corfu-terminal)
 (setq completion-cycle-threshold 3)
 (global-corfu-mode)
 (with-eval-after-load 'corfu
-  (define-key corfu-map (kbd "<tab>") 'corfu-next)
-  (define-key corfu-map (kbd "<backtab>") 'corfu-previous))
+  (define-key corfu-map (kbd "TAB") 'corfu-next)
+  (define-key corfu-map (kbd "S-TAB") 'corfu-previous))
+(unless (display-graphic-p)
+  (corfu-terminal-mode +1))
 
 (provide 'completion)
