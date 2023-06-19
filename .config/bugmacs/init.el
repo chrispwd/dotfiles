8941ae8e5bfb4d81baea839636ea3f9e1a792e22
chrispwd <chrispwd@protonmail.com>
2023-06-19 17:44:24 -0600

    wip chrispwdmacs; tmux prefix change

--- .config/bugmacs/init.el
+++ .config/bugmacs/init.el
@@ -1,24 +1,44 @@
 ;;; Bugmacs: for bugtesting
-
+;;         __         _                        ____  ___
+;;   _____/ /_  _____(_)________ _      ______/ /  |/  /___ ___________
+;;  / ___/ __ \/ ___/ / ___/ __ \ | /| / / __  / /|_/ / __ `/ ___/ ___/
+;; / /__/ / / / /  / (__  ) /_/ / |/ |/ / /_/ / /  / / /_/ / /__(__  )
+;; \___/_/ /_/_/  /_/____/ .___/|__/|__/\__,_/_/  /_/\__,_/\___/____/
+;;                      /_/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;            BEGIN DEFAULTS          ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'package)
-(when (version< emacs-version "28")
-  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/")))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
+;; (require 'package)
+;; (when (version< emacs-version "28")
+;;   (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
 
-(defun bm-package-initialize ()
-  (package-initialize)
-  (unless package-archive-contents
-    (package-refresh-contents)))
+;; (defun bm-package-initialize ()
+;;   (package-initialize)
+;;   (unless package-archive-contents
+;;     (package-refresh-contents)))
 
-(bm-package-initialize)
+;; (bm-package-initialize)
 
-(defmacro bm-install-package-if-not-already (package)
-  "Only install the package if it is not already installed."
-  `(unless (package-installed-p ,package) (package-install ,package)))
+;; (defmacro bm-install-package-if-not-already (package)
+;;   "Only install the package if it is not already installed."
+;;   `(unless (package-installed-p ,package) (package-install ,package)))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
 
 (load-theme 'modus-vivendi t)
 (add-to-list 'custom-theme-load-path "~/.config/bugmacs/themes")
@@ -58,63 +78,105 @@
 ;; move auto-backups to one place
 (setq backup-directory-alist `(("." . "~/.saves/")))
 
-(bm-install-package-if-not-already 'consult)
-(bm-install-package-if-not-already 'vertico)
-(bm-install-package-if-not-already 'savehist)
-(bm-install-package-if-not-already 'marginalia)
-(bm-install-package-if-not-already 'orderless)
+(straight-use-package 'consult)
+(straight-use-package 'vertico)
+(straight-use-package 'savehist)
+(straight-use-package 'marginalia)
+(straight-use-package 'orderless)
 
-(require 'vertico)
-(vertico-mode)
-(setq vertico-cycle t)
+(use-package vertico
+             :straight t
+             :init
+             (setq vertico-cycle t)
+             :config
+             (vertico-mode))
 
-(require 'savehist)
-(savehist-mode)
+(use-package savehist
+             :straight t
+             :config
+             (savehist-mode))
 
-(require 'marginalia)
-(with-eval-after-load 'vertico
-  (marginalia-mode))
+(use-package marginalia
+             :straight t
+             :after (vertico)
+             :config
+             (marginalia-mode))
 
-(require 'consult)
-(setq completion-styles '(substring basic))
-(global-set-key (kbd "C-s") 'consult-line)
-(global-set-key (kbd "C-M-y") 'consult-yank-from-kill-ring)
+;; (require 'marginalia)
+;; (with-eval-after-load 'vertico
+;;   (marginalia-mode))
 
-(require 'orderless)
-(setq completion-styles '(orderless basic)
-      completion-category-overrides '((file (styles basic partial-completion))))
+(use-package consult
+             :straight t
+             :init
+             (setq completion-styles '(substring basic))
+             :bind
+             (("C-s" . consult-line)                             
+              ("C-M-y" . consult-yank-from-kill-ring)))
+
+;; (require 'consult)
+;; (setq completion-styles '(substring basic))
+;; (global-set-key (kbd "C-s") 'consult-line)
+;; (global-set-key (kbd "C-M-y") 'consult-yank-from-kill-ring)
+
+(use-package orderless
+             :straight t
+             :init
+             (setq completion-styles '(orderless basic)
+                   completion-category-overrides '((file (styles basic partial-completion)))))
+
+;; (require 'orderless)
+;; (setq completion-styles '(orderless basic)
+;;       completion-category-overrides '((file (styles basic partial-completion))))
+
+(use-package evil
+             :straight t
+             :init
+             (setq evil-want-C-u-scroll t)
+             (setq evil-want-Y-yank-to-eol t)
+             (setq evil-normal-state-cursor '(box)
+                   evil-insert-state-cursor '(bar)
+                   evil-visual-state-cursor '(hollow))
+             :config
+             (evil-mode 1))
+
+;; Add the modules folder to the load path
+(add-to-list 'load-path (expand-file-name "modules/" user-emacs-directory))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;            END DEFAULTS            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(bm-install-package-if-not-already 'nix-mode)
-;(bm-install-package-if-not-already 'clipetty)
-(bm-install-package-if-not-already 'xclip)
-(bm-install-package-if-not-already 'evil)
+(require 'coding)
+(require 'look-and-feel)
+(require 'os-specific)
+;; (bm-install-package-if-not-already 'nix-mode)
+;; ;(bm-install-package-if-not-already 'clipetty)
+;; (bm-install-package-if-not-already 'xclip)
+;; (bm-install-package-if-not-already 'evil)
 
-(require 'nix-mode)
-(with-eval-after-load 'nix-mode 
-  (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode)))
+;; (require 'nix-mode)
+;; (with-eval-after-load 'nix-mode 
+;;   (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode)))
 
-(org-babel-do-load-languages 'org-babel-load-languages
-                             (append org-babel-load-languages
-                              '((sql         . t)
-                                (shell       . t))))
+;; (org-babel-do-load-languages 'org-babel-load-languages
+;;                              (append org-babel-load-languages
+;;                               '((sql         . t)
+;;                                 (shell       . t))))
 
-(require 'ox-md)
+;; (require 'ox-md)
 
-;(require 'clipetty)
-(require 'xclip)
-(xclip-mode 1)
+;; ;(require 'clipetty)
+;; (require 'xclip)
+;; (xclip-mode 1)
 ;(global-clipetty-mode)
 ;(setq clipetty-tmux-ssh-tty "tmux show-environment SSH_TTY")
 
-(setq evil-want-C-u-scroll t)
-(setq evil-want-Y-yank-to-eol t)
-(setq evil-normal-state-cursor '(box)
-      evil-insert-state-cursor '(bar)
-      evil-visual-state-cursor '(hollow))
-(require 'evil)
-(evil-mode 1)
+;; (setq evil-want-C-u-scroll t)
+;; (setq evil-want-Y-yank-to-eol t)
+;; (setq evil-normal-state-cursor '(box)
+;;       evil-insert-state-cursor '(bar)
+;;       evil-visual-state-cursor '(hollow))
+;; (require 'evil)
+;; (evil-mode 1)
 
