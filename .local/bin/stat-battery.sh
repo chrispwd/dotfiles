6a899251defcba25cf8cb3dd40dceec8a9ce01d1
chrispwd <chrispwd@protonmail.com>
2023-07-31 01:07:05 -0600

    cleanup battery script

--- .local/bin/stat-battery.sh
+++ .local/bin/stat-battery.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
-test2=""
-test3="35"
-for test in $outputs; do
-    printf "[%s %s%d%%] " "$test" "$warn" "$test3"; unset test2
-done && printf "\\n"
+# outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+# test2=""
+# test3="35"
+# for test in $outputs; do
+#     printf "[%s %s%d%%] " "$test" "$warn" "$test3"; unset test2
+# done && printf "\\n"
 
-[ ! -f /sys/class/power_supply/BAT0 ] && exit 0
+[ ! -h /sys/class/power_supply/BAT0 ] && exit 0
 # Loop through all attached batteries and format the info
 for battery in /sys/class/power_supply/BAT?*; do
 	# If non-first battery, print a space separator.
 	[ -n "${capacity+x}" ] && printf " "
 	# Sets up the status and capacity
+        batname="$(basename $battery)"
 	case "$(cat "$battery/status" 2>&1)" in
-		"Full") status="$battery " ;;
-		"Discharging") status="-$battery " ;;
-		"Charging") status="+$battery " ;;
-		"Not charging") status="x$battery " ;;
-		"Unknown") status="?$battery ️" ;;
+		"Full") status="$batname " ;;
+		"Discharging") status="-$batname " ;;
+		"Charging") status="+$batname " ;;
+		"Not charging") status="$batname " ;;
+		"Unknown") status="?$batname ️" ;;
 		*) echo "$(cat "$battery/status")" && exit 1 ;;
 	esac
 	capacity="$(cat "$battery/capacity" 2>&1)"
 	# Will make a warn variable if discharging and low
-	[ "$status" = "-$battery " ] && [ "$capacity" -le 25 ] && warn="!"
+	[ "$status" = "-$batname " ] && [ "$capacity" -le 25 ] && warn="!"
 	# Prints the info
-	printf "[%s%s%d%%] " "$status" "$warn" "$capacity"; unset warn
+	printf "[%s%s%d%%]" "$status" "$warn" "$capacity"; unset warn
 done && printf "\\n"
