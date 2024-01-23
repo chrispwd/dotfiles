8a7cb038caab61d7493652df7859205f140c0ab5
chrispwd <chrispwd@protonmail.com>
2024-01-23 06:36:48 -0700

    fix/typo

--- .config/mimemacs/modules/look-and-feel.el
+++ .config/mimemacs/modules/look-and-feel.el
@@ -93,7 +93,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-tomorrow-dark t))
+  (load-theme 'doom-tomorrow-night t))
 
 ;; Module interoperability
 (add-hook 'ef-themes-post-load-hook #'fontaine-apply-current-preset)
