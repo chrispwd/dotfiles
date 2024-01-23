951d23dcd09cf4d8c225159f3d02de272d17b9ed
chrispwd <chrispwd@protonmail.com>
2024-01-23 06:35:33 -0700

    ect/change theme

--- .config/mimemacs/modules/look-and-feel.el
+++ .config/mimemacs/modules/look-and-feel.el
@@ -93,7 +93,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-solarized-dark t))
+  (load-theme 'doom-tomorrow-dark t))
 
 ;; Module interoperability
 (add-hook 'ef-themes-post-load-hook #'fontaine-apply-current-preset)
