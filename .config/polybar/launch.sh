850682ed3da95f414efdf6099924dfcb80090ff2
chrispwd <chrispwd@protonmail.com>
2024-05-19 23:50:25 -0600

    fix: hotplug of external monitor debug

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,6 +11,7 @@ else
 fi
 
 outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+echo "[$(date)]: (POLYBAR) outputs: $outputs" >> /tmp/bspwm-log
 
 for m in $outputs ; do
     MONITOR=$m MOUNTPOINT=$mount polybar --reload mybar > ~/.cache/polybar/$m.log 2>&1 &
