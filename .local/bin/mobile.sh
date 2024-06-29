b2dab8bb9009fe5cd6c96032ae37eb58a3d3752c
chrispwd <chrispwd@protonmail.com>
2024-06-28 21:49:24 -0600

    fix: remove keepass as sticky group, fix mobile key setup

--- .local/bin/mobile.sh
+++ .local/bin/mobile.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+#setxkbmap -option ctrl:ralt_rctrl
 setxkbmap -option ctrl:nocaps
+#setxkbmap -option caps:swapescape
 # # Toggles colemak-dh and switches capslock to control
 # if [ $# -eq 0 ]
 # then
