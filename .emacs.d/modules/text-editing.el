b175fcdbf33415eea0404562737a0924b35c10ea
chrispwd <chrispwd@protonmail.com>
2024-09-15 22:52:14 -0600

    feat/Lord forgive me but its time to go back to the evil-mode me

--- .emacs.d/modules/text-editing.el
+++ .emacs.d/modules/text-editing.el
@@ -5,6 +5,7 @@
 (cpwd/install-if-not 'avy)
 (cpwd/install-if-not 'evil)
 (cpwd/install-if-not 'evil-collection)
+(cpwd/install-if-not 'evil-terminal-cursor-changer)
 
 (use-package avy
   :ensure t
@@ -15,27 +16,41 @@
   (global-set-key (kbd "M-g w") 'avy-goto-word-1)
   (global-set-key (kbd "M-g e") 'avy-goto-word-0))
 
-;; (use-package evil
-;;   :ensure t
-;;   :init
-;;   (setq evil-want-integration t)
-;;   (setq evil-want-keybinding nil)
-;;   (setq evil-want-C-u-scroll t)
-;;   (setq evil-want-Y-yank-to-eol t)
-;;   (setq evil-motion-state-cursor 'box)  ; █
-;;   (setq evil-visual-state-cursor 'box)  ; █
-;;   (setq evil-normal-state-cursor 'box)  ; █
-;;   ;; (setq evil-insert-state-cursor 'bar)  ; ⎸
-;;   (setq evil-insert-state-cursor 'hbar) ; _
-;;   (setq evil-emacs-state-cursor  'hbar) ; _
-;;   :config
-;;   (evil-mode 1)
-;;   (evil-set-leader 'normal (kbd "SPC")))
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-Y-yank-to-eol t)
+  (setq evil-motion-state-cursor 'box)  ; █
+  (setq evil-visual-state-cursor 'box)  ; █
+  (setq evil-normal-state-cursor 'box)  ; █
+  (setq evil-insert-state-cursor 'bar)  ; ⎸
+  ;(setq evil-insert-state-cursor 'hbar) ; _
+  (setq evil-emacs-state-cursor  'hbar) ; _
+  :config
+  (evil-mode 1)
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-define-key
+    '(normal insert visual replace operator motion emacs)
+    'global
+    (kbd "C-n") nil)
+  (evil-define-key
+    '(normal insert visual replace operator motion emacs)
+    'global
+    (kbd "C-p") nil))
 
-;; (use-package evil-collection
-;;   :ensure t
-;;   :after evil
-;;   :config
-;;   (evil-collection-init))
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (evil-collection-init))
+
+(unless (display-graphic-p)
+  (use-package evil-terminal-cursor-changer
+    :ensure t
+    :config
+    (evil-terminal-cursor-changer-activate)))
 
 (provide 'text-editing)
