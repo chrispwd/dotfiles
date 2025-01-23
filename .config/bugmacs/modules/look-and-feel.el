c188d08eeb5cf37041e6fb4392d7fdf1d5bce917
chrispwd <chrispwd@protonmail.com>
2025-01-22 22:08:57 -0700

    init testing

--- .config/bugmacs/modules/look-and-feel.el
+++ .config/bugmacs/modules/look-and-feel.el
@@ -9,6 +9,9 @@
 (straight-use-package 'ef-themes)
 (straight-use-package 'autothemer)
 
+;; Custom themes dir
+(add-to-list 'custom-theme-load-path "~/.config/bugmacs/themes")
+
 ;; Theme
 (use-package all-the-icons
              :straight t)
@@ -35,76 +38,84 @@
              (customize-set-variable 'doom-modeline-minor-modes t)
              (customize-set-variable 'doom-modeline-buffer-file-name-style 'truncate-except-project))
 
-(use-package fontaine
-  :straight t
-  :init
-  (setq fontaine-latest-state-file
-      (locate-user-emacs-file "fontaine-latest-state.eld"))
-  (setq fontaine-presets
-      '((tiny
-         :default-family "Fixed (OTB)"
-         :default-height 80)
-        (regular
-         :default-height 120)
-        (medium
-         :default-height 140)
-        (large
-         :default-weight semilight
-         :default-height 160
-         :bold-weight extrabold)
-        (presentation
-         :default-weight semilight
-         :default-height 180
-         :bold-weight extrabold)
-        (jumbo
-         :default-weight semilight
-         :default-height 220
-         :bold-weight extrabold)
-        (t
-         ;; I keep all properties for didactic purposes, but most can be
-         ;; omitted.  See the fontaine manual for the technicalities:
-         ;; <https://protesilaos.com/emacs/fontaine>.
-         :default-family "Iosevka Term SS12"
-         :default-weight regular
-         :default-height 120
-         :fixed-pitch-family nil ; falls back to :default-family
-         :fixed-pitch-weight nil ; falls back to :default-weight
-         :fixed-pitch-height 1.0
-         :fixed-pitch-serif-family nil ; falls back to :default-family
-         :fixed-pitch-serif-weight nil ; falls back to :default-weight
-         :fixed-pitch-serif-height 1.0
-         :variable-pitch-family "Iosevka Term SS12"
-         :variable-pitch-weight nil
-         :variable-pitch-height 1.0
-         :bold-family nil ; use whatever the underlying face has
-         :bold-weight bold
-         :italic-family nil
-         :italic-slant italic
-         :line-spacing nil)))
-  :config
-  (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))
-  :catch (lambda (keyword err)
-           (message (error-message-string err)))
-  :hook
-  (kill-emacs . fontaine-store-latest-preset))
 
-(use-package ef-themes
-  :straight t
-  :init
-  (setq ef-themes-headings ; read the manual's entry or the doc string
-      '((0 variable-pitch 1.3)
-        (1 variable-pitch 1.2)
-        (2 variable-pitch 1.0)
-        (3 variable-pitch 1.0)
-        (4 variable-pitch 1.0)
-        (5 variable-pitch 1.0) ; absence of weight means `bold'
-        (6 variable-pitch 1.0)
-        (7 variable-pitch 1.0)
-        (t variable-pitch 1.0)))
-  :config
-  ;;(mapc #'disable-theme custom-enabled-themes)
-  (ef-themes-select 'ef-dark)
-  )
+(load-theme 'twilight t)
+;; (use-package doom-themes
+;;   :ensure t
+;;   ;; :init
+;;   ;; (setq doom-themes-enable-bold nil)
+;;   :config
+;;   (load-theme 'doom-twilight t))
+;; (use-package fontaine
+;;   :straight t
+;;   :init
+;;   (setq fontaine-latest-state-file
+;;       (locate-user-emacs-file "fontaine-latest-state.eld"))
+;;   (setq fontaine-presets
+;;       '((tiny
+;;          :default-family "Fixed (OTB)"
+;;          :default-height 80)
+;;         (regular
+;;          :default-height 120)
+;;         (medium
+;;          :default-height 140)
+;;         (large
+;;          :default-weight semilight
+;;          :default-height 160
+;;          :bold-weight extrabold)
+;;         (presentation
+;;          :default-weight semilight
+;;          :default-height 180
+;;          :bold-weight extrabold)
+;;         (jumbo
+;;          :default-weight semilight
+;;          :default-height 220
+;;          :bold-weight extrabold)
+;;         (t
+;;          ;; I keep all properties for didactic purposes, but most can be
+;;          ;; omitted.  See the fontaine manual for the technicalities:
+;;          ;; <https://protesilaos.com/emacs/fontaine>.
+;;          :default-family "Iosevka Term SS12"
+;;          :default-weight regular
+;;          :default-height 120
+;;          :fixed-pitch-family nil ; falls back to :default-family
+;;          :fixed-pitch-weight nil ; falls back to :default-weight
+;;          :fixed-pitch-height 1.0
+;;          :fixed-pitch-serif-family nil ; falls back to :default-family
+;;          :fixed-pitch-serif-weight nil ; falls back to :default-weight
+;;          :fixed-pitch-serif-height 1.0
+;;          :variable-pitch-family "Iosevka Term SS12"
+;;          :variable-pitch-weight nil
+;;          :variable-pitch-height 1.0
+;;          :bold-family nil ; use whatever the underlying face has
+;;          :bold-weight bold
+;;          :italic-family nil
+;;          :italic-slant italic
+;;          :line-spacing nil)))
+;;   :config
+;;   (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))
+;;   :catch (lambda (keyword err)
+;;            (message (error-message-string err)))
+;;   :hook
+;;   (kill-emacs . fontaine-store-latest-preset))
+
+;; (use-package ef-themes
+;;   :straight t
+;;   :init
+;;   (setq ef-themes-headings ; read the manual's entry or the doc string
+;;       '((0 variable-pitch 1.3)
+;;         (1 variable-pitch 1.2)
+;;         (2 variable-pitch 1.0)
+;;         (3 variable-pitch 1.0)
+;;         (4 variable-pitch 1.0)
+;;         (5 variable-pitch 1.0) ; absence of weight means `bold'
+;;         (6 variable-pitch 1.0)
+;;         (7 variable-pitch 1.0)
+;;         (t variable-pitch 1.0)))
+;;   :config
+;;   ;;(mapc #'disable-theme custom-enabled-themes)
+;;   (ef-themes-select 'ef-dark)
+;;   )
 ;; Fonts and Faces
 ;; (cond
 ;;   ((eq system-type 'gnu/linux)
@@ -116,32 +127,32 @@
 ;;   (t
 ;;     (message "Config for unknown system type.")))
 
-(set-face-attribute 'default nil
-                    :font "Iosevka Term SS12"
-                    :height 120
-                    :slant 'normal)
-(set-face-attribute 'bold nil
-                    :inherit 'default
-                    :weight 'bold)
-(set-face-attribute 'font-lock-builtin-face nil
-                    :font "Iosevka Term SS12 12"
-                    :slant 'normal)
-(set-face-attribute 'font-lock-constant-face nil
-                    :inherit 'default
-                    :weight 'normal)
-;; (set-face-attribute 'font-lock-comment-face nil
-;;                     :font "Iosevka Term SS12 Light Oblique 12"
-;;                     :slant 'oblique)
-(set-face-attribute 'font-lock-preprocessor-face nil
-                    :weight 'normal)
-(set-face-attribute 'font-lock-keyword-face nil
-                    :weight 'normal)
-(set-face-attribute 'font-lock-type-face nil
-                    :inherit 'default
-                    :slant 'normal)
-(set-face-attribute 'font-lock-function-name-face nil
-                    :font "Iosevka Term SS12 12"
-                    :weight 'light)
+;; (set-face-attribute 'default nil
+;;                     :font "Iosevka Term SS12"
+;;                     :height 120
+;;                     :slant 'normal)
+;; (set-face-attribute 'bold nil
+;;                     :inherit 'default
+;;                     :weight 'bold)
+;; (set-face-attribute 'font-lock-builtin-face nil
+;;                     :font "Iosevka Term SS12 12"
+;;                     :slant 'normal)
+;; (set-face-attribute 'font-lock-constant-face nil
+;;                     :inherit 'default
+;;                     :weight 'normal)
+;; ;; (set-face-attribute 'font-lock-comment-face nil
+;; ;;                     :font "Iosevka Term SS12 Light Oblique 12"
+;; ;;                     :slant 'oblique)
+;; (set-face-attribute 'font-lock-preprocessor-face nil
+;;                     :weight 'normal)
+;; (set-face-attribute 'font-lock-keyword-face nil
+;;                     :weight 'normal)
+;; (set-face-attribute 'font-lock-type-face nil
+;;                     :inherit 'default
+;;                     :slant 'normal)
+;; (set-face-attribute 'font-lock-function-name-face nil
+;;                     :font "Iosevka Term SS12 12"
+;;                     :weight 'light)
 ;; (set-face-attribute 'eglot-highlight-symbol-face nil
 ;;                     :font "Iosevka Term SS12 Semibold 12"
 ;;                     :weight 'semi-bold)
--- .config/bugmacs/modules/text-editing.el
+++ .config/bugmacs/modules/text-editing.el
@@ -1,40 +1,56 @@
 ;;; text-editing.el -*- lexical-binding: t; -*-
 
 ;;; TODO:: finish
-(straight-use-package 'multiple-cursors)
+;; (straight-use-package 'multiple-cursors)
 (straight-use-package 'evil)
 (straight-use-package 'evil-collection)
-(straight-use-package 'god-mode)
+;; (straight-use-package 'god-mode)
 (straight-use-package 'avy)
+(straight-use-package 'evil-terminal-cursor-changer)
 
-;; (use-package evil
-;;   :straight t
-;;   :init
-;;   (setq evil-want-integration t)
-;;   (setq evil-want-keybinding nil)
-;;   (setq evil-want-C-u-scroll t)
-;;   (setq evil-want-Y-yank-to-eol t)
-;;   (setq evil-motion-state-cursor 'box)  ; █
-;;   (setq evil-visual-state-cursor 'box)  ; █
-;;   (setq evil-normal-state-cursor 'box)  ; █
-;;   (setq evil-insert-state-cursor 'bar)  ; ⎸
-;;   (setq evil-emacs-state-cursor  'hbar) ; _
-;;   :config
-;;   (evil-mode 1))
-;;   ;; (define-key evil-normal-state-map (kbd "TAB") 'indent-for-tab-command)
-;;   ;; (define-key evil-visual-state-map (kbd "TAB") 'indent-for-tab-command))
-
-;; (use-package evil-collection
-;;   :straight t
-;;   :after evil
-;;   :config
-;;   (evil-collection-init))
-
-(use-package god-mode
+(use-package evil
   :straight t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-Y-yank-to-eol t)
+  (setq evil-motion-state-cursor 'box)  ; █
+  (setq evil-visual-state-cursor 'box)  ; █
+  (setq evil-normal-state-cursor 'box)  ; █
+  (setq evil-insert-state-cursor 'bar)  ; ⎸
+  ;(setq evil-insert-state-cursor 'hbar) ; _
+  (setq evil-emacs-state-cursor  'hbar) ; _
+  (setq evil-undo-system 'undo-redo)
   :config
-  (global-set-key (kbd "<escape>") #'god-mode-all)
-  (god-mode))
+  (evil-mode 1)
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-define-key
+    '(normal insert visual replace operator motion emacs)
+    'global
+    (kbd "C-n") nil)
+  (evil-define-key
+    '(normal insert visual replace operator motion emacs)
+    'global
+    (kbd "C-p") nil))
+
+(use-package evil-collection
+  :straight t
+  :after evil
+  :config
+  (evil-collection-init))
+
+(unless (display-graphic-p)
+  (use-package evil-terminal-cursor-changer
+    :straight t
+    :config
+    (evil-terminal-cursor-changer-activate)))
+
+;; (use-package god-mode
+;;   :straight t
+;;   :config
+;;   (global-set-key (kbd "<escape>") #'god-mode-all)
+;;   (god-mode))
 
 (use-package avy
   :straight t
