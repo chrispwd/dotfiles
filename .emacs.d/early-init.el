71a6a8a5a6f06203cbf9a19080f7c01dd99cc02e
chrispwd <chrispwd@protonmail.com>
2025-04-05 20:51:50 -0600

    fix: typo in early-init.el

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -22,7 +22,7 @@
 
 ;; Disables unused UI Elements
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'scoll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'tooltip-mode) (tooltip-mode -1))
 
