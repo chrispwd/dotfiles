d986adcfa34c5ff59dd36284d70050d423f0f244
chrispwd <chrispwd@protonmail.com>
2025-10-16 02:21:57 -0600

    feat: ok I lied gruvbox is nice too

--- emacs/.emacs.d/modules/style-config.el
+++ emacs/.emacs.d/modules/style-config.el
@@ -209,48 +209,96 @@
 ;;           bold)))))
 ;;   )
 
+;; Swap commented palette and theme name in `load-theme' to switch
+;; between solarized and gruvbox
 (use-package solarized-theme
   :ensure t
   :config
   (setq solarized-use-less-bold t)
+  ;; (defvar palette
+  ;; '(;; solarized-dark palette
+  ;;   (base03      . "#002b36")
+  ;;   (base02      . "#073642")
+  ;;   (base01      . "#586e75")
+  ;;   (base00      . "#657b83")
+  ;;   (base0       . "#839496")
+  ;;   (base1       . "#93a1a1")
+  ;;   (base2       . "#eee8d5")
+  ;;   (base3       . "#fdf6e3")
+  ;;   (yellow      . "#b58900")
+  ;;   (orange      . "#cb4b16")
+  ;;   (red         . "#dc322f")
+  ;;   (magenta     . "#d33682")
+  ;;   (violet      . "#6c71c4")
+  ;;   (blue        . "#268bd2")
+  ;;   (cyan        . "#2aa198")
+  ;;   (green       . "#859900")
+  ;;   ;; palette end
+  ;;   )
+  ;; "The solarized color palette alist.")
   (defvar palette
-  '(;; solarized-dark palette
-    (base03      . "#002b36")
-    (base02      . "#073642")
-    (base01      . "#586e75")
-    (base00      . "#657b83")
-    (base0       . "#839496")
-    (base1       . "#93a1a1")
-    (base2       . "#eee8d5")
-    (base3       . "#fdf6e3")
-    (yellow      . "#b58900")
-    (orange      . "#cb4b16")
-    (red         . "#dc322f")
-    (magenta     . "#d33682")
-    (violet      . "#6c71c4")
-    (blue        . "#268bd2")
-    (cyan        . "#2aa198")
-    (green       . "#859900")
+    '(;; gruvbox-dark palette
+    (base03      . "#282828")
+    (base02      . "#32302f")
+    (base01      . "#7c6f64")
+    (base00      . "#928374")
+    (base0       . "#a89984")
+    (base1       . "#bdae93")
+    (base2       . "#a89984")
+    (base3       . "#fbf1c7")
+    (yellow      . "#d79921")
+    (orange      . "#d65d0e")
+    (red         . "#fb4933")
+    (magenta     . "#d3869b")
+    (violet      . "#b16286")
+    (blue        . "#458588")
+    (cyan        . "#689d6a")
+    (green       . "#98971a")
     ;; palette end
     )
-  "The solarized color palette alist.")
-  (load-theme 'solarized-dark t)
+    "The gruvbox color palette alist.")
+  (load-theme 'solarized-gruvbox-dark t)
+  ;; (custom-set-faces
+  ;;  `(font-lock-variable-name-face ((t (:foreground ,(alist-get 'base0 palette)))))
+  ;;  `(font-lock-builtin-face ((t (:foreground ,(alist-get 'yellow palette)))))
+  ;;  `(font-lock-number-face ((t (:foreground ,(alist-get 'magenta palette)))))
+  ;;  `(font-lock-operator-face ((t (:foreground ,(alist-get 'base00 palette)))))
+  ;;  `(font-lock-constant-face
+  ;;    ((t (:foreground ,(alist-get 'orange palette) :weight normal))))
+  ;;  `(flymake-error ((t (:foreground ,(alist-get 'red palette)))))
+  ;;  `(bold
+  ;;    ((t (:foreground ,(alist-get 'base1 palette) :weight bold))))
+  ;;  `(denote-faces-delimiter
+  ;;    ((t (:foreground ,(alist-get 'base02 palette)))))
+  ;;  `(denote-faces-date ((t (:inherit shadow))))
+  ;;  `(org-list-dt
+  ;;    ((t (:foreground ,(alist-get 'orange palette) :weight normal :slant italic))))
+  ;;  `(org-code ((t (:foreground ,(alist-get 'green palette))))))
   (custom-set-faces
    `(font-lock-variable-name-face ((t (:foreground ,(alist-get 'base0 palette)))))
-   `(font-lock-builtin-face ((t (:foreground ,(alist-get 'yellow palette)))))
-   `(font-lock-number-face ((t (:foreground ,(alist-get 'magenta palette)))))
-   `(font-lock-operator-face ((t (:foreground ,(alist-get 'base00 palette)))))
+   `(font-lock-keyword-face ((t (:foreground ,(alist-get 'red palette)))))
+   `(font-lock-builtin-face ((t (:foreground ,(alist-get 'violet palette)))))
+   `(font-lock-number-face ((t (:foreground ,(alist-get 'violet palette)))))
+   `(font-lock-string-face ((t (:foreground ,(alist-get 'green palette)))))
+   `(font-lock-punctuation-face ((t (:foreground ,(alist-get 'orange palette)))))
+   `(font-lock-function-name-face ((t (:foreground ,(alist-get 'blue palette)))))
+   `(font-lock-function-call-face ((t (:foreground ,(alist-get 'base0 palette)))))
+   `(font-lock-operator-face ((t (:foreground ,(alist-get 'orange palette)))))
+   `(font-lock-type-face ((t (:foreground ,(alist-get 'yellow palette)))))
    `(font-lock-constant-face
-     ((t (:foreground ,(alist-get 'orange palette) :weight normal))))
-   `(flymake-error ((t (:foreground ,(alist-get 'red palette)))))
+     ((t (:foreground ,(alist-get 'violet palette) :weight normal))))
+   `(flymake-error ((t (:foreground ,(alist-get 'red palette) :underline t))))
+   `(link
+     ((t (:foreground ,(alist-get 'yellow palette) :weight normal :underline t))))
    `(bold
      ((t (:foreground ,(alist-get 'base1 palette) :weight bold))))
    `(denote-faces-delimiter
      ((t (:foreground ,(alist-get 'base02 palette)))))
    `(denote-faces-date ((t (:inherit shadow))))
    `(org-list-dt
-     ((t (:foreground ,(alist-get 'cyan palette) :weight normal :slant italic))))
-   `(org-code ((t (:foreground ,(alist-get 'green palette)))))))
+     ((t (:foreground ,(alist-get 'orange palette) :weight normal :slant italic))))
+   `(org-code ((t (:foreground ,(alist-get 'orange palette))))))
+  )
 
 ;; (use-package doom-themes
 ;;   :ensure t
