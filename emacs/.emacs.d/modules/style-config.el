0c98ace0b7648c41dca37390696bf1b8205cdf36
chrispwd <chrispwd@protonmail.com>
2026-01-08 00:03:16 -0700

    fix: dunst msg geometry, feat: cascadia, cpl theme

--- emacs/.emacs.d/modules/style-config.el
+++ emacs/.emacs.d/modules/style-config.el
@@ -7,92 +7,153 @@
 
 ;; (load-theme 'zenburn t)
 
-;; Swap commented palette and theme name in `load-theme' to switch
-;; between solarized and gruvbox
-(use-package solarized-theme
+(use-package base16-theme
   :ensure t
   :config
-  (setq solarized-use-less-bold t)
-  (defvar palette
-  '(;; solarized-dark palette
-    (base03      . "#002b36")
-    (base02      . "#073642")
-    (base01      . "#586e75")
-    (base00      . "#657b83")
-    (base0       . "#839496")
-    (base1       . "#93a1a1")
-    (base2       . "#eee8d5")
-    (base3       . "#fdf6e3")
-    (yellow      . "#b58900")
-    (orange      . "#cb4b16")
-    (red         . "#dc322f")
-    (magenta     . "#d33682")
-    (violet      . "#6c71c4")
-    (blue        . "#268bd2")
-    (cyan        . "#2aa198")
-    (green       . "#859900")
-    ;; palette end
-    )
-  "The solarized color palette alist.")
+  (setq base16-theme-256-color-source 'colors)
+  (load-theme 'base16-cpl-custom t)
+  (defvar my/base16-colors
+  '(:base00 "#000000"
+    :base01 "#1d1d1d"
+    :base02 "#232323"
+    :base03 "#424242"
+    :base04 "#585858"
+    :base05 "#777777"
+    :base06 "#989898"
+    :base07 "#ababab"
+    :red "#b10b00"
+    :orange "#a05803"
+    :yellow "#a27f00"
+    :green "#297200"
+    :cyan "#00808f"
+    :blue "#006dbd"
+    :violet "#ae3aab"
+    :brown "#7a2d00")
+  "All colors for Base16 CPL are defined here.")
   
-  (load-theme 'solarized-dark t)
-
-  ;; faces for gruvbox
-  ;; (custom-set-faces
-  ;;    `(font-lock-variable-name-face ((t (:foreground ,(alist-get 'base0 palette)))))
-  ;;  `(font-lock-keyword-face ((t (:foreground ,(alist-get 'red palette)))))
-  ;;  `(font-lock-builtin-face ((t (:foreground ,(alist-get 'violet palette)))))
-  ;;  `(font-lock-number-face ((t (:foreground ,(alist-get 'violet palette)))))
-  ;;  `(font-lock-string-face ((t (:foreground ,(alist-get 'green palette)))))
-  ;;  `(font-lock-punctuation-face ((t (:foreground ,(alist-get 'orange palette)))))
-  ;;  `(font-lock-function-name-face ((t (:foreground ,(alist-get 'blue palette)))))
-  ;;  `(font-lock-function-call-face ((t (:foreground ,(alist-get 'base0 palette)))))
-  ;;  `(font-lock-operator-face ((t (:foreground ,(alist-get 'orange palette)))))
-  ;;  `(font-lock-type-face ((t (:foreground ,(alist-get 'yellow palette)))))
-  ;;  `(font-lock-constant-face
-  ;;    ((t (:foreground ,(alist-get 'violet palette) :weight normal))))
-  ;;  `(flymake-error ((t (:foreground ,(alist-get 'red palette) :underline t))))
-  ;;  `(link
-  ;;    ((t (:foreground ,(alist-get 'yellow palette) :weight normal :underline t))))
-  ;;  `(bold
-  ;;    ((t (:foreground ,(alist-get 'base1 palette) :weight bold))))
-  ;;  `(vertical-border ((t (:foreground ,(alist-get 'base01 palette)))))
-  ;;  `(denote-faces-delimiter
-  ;;    ((t (:foreground ,(alist-get 'base02 palette)))))
-  ;;  `(denote-faces-date ((t (:inherit shadow))))
-  ;;  `(org-list-dt
-  ;;    ((t (:foreground ,(alist-get 'orange palette) :weight normal :slant italic))))
-  ;;  `(org-code ((t (:foreground ,(alist-get 'orange palette))))))
-
-  ;; faces for solarized
   (custom-set-faces
-   `(font-lock-variable-name-face ((t (:foreground ,(alist-get 'base0 palette)))))
-   `(font-lock-keyword-face ((t (:foreground ,(alist-get 'green palette)))))
-   `(font-lock-builtin-face ((t (:foreground ,(alist-get 'violet palette)))))
-   ;; `(font-lock-number-face ((t (:foreground ,(alist-get 'violet palette)))))
-   `(font-lock-string-face ((t (:foreground ,(alist-get 'cyan palette)))))
-   `(font-lock-function-name-face ((t (:foreground ,(alist-get 'blue palette)))))
-   `(font-lock-function-call-face ((t (:foreground ,(alist-get 'blue palette)))))
-   ;; `(font-lock-operator-face ((t (:foreground ,(alist-get 'orange palette)))))
-   ;; `(font-lock-punctuation-face ((t (:foreground ,(alist-get 'orange palette)))))
-   `(font-lock-type-face ((t (:foreground ,(alist-get 'yellow palette)))))
-   `(font-lock-constant-face
-     ((t (:foreground ,(alist-get 'yellow palette) :weight normal))))
-   `(eglot-highlight-symbol-face
-     ((t (:foreground ,(alist-get 'base2 palette) :weight bold))))
-   `(flymake-error ((t (:foreground ,(alist-get 'red palette) :underline t))))
+   `(line-number
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base02)
+          :background
+          ,(plist-get my/base16-colors :base00)))))
    `(link
-     ((t (:foreground ,(alist-get 'yellow palette) :weight normal :underline t))))
-   `(bold
-     ((t (:weight bold))))
-   `(vertical-border ((t (:foreground ,(alist-get 'base01 palette)))))
-   `(denote-faces-delimiter
-     ((t (:foreground ,(alist-get 'base02 palette)))))
-   `(denote-faces-date ((t (:inherit shadow))))
-   `(org-list-dt
-     ((t (:foreground ,(alist-get 'orange palette) :weight normal :slant italic))))
-   `(org-code ((t (:foreground ,(alist-get 'orange palette))))))
-  )
+     ((t (:foreground
+          ,(plist-get my/base16-colors :cyan)
+          :underline t))))
+   `(tab-bar-tab
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base07)
+          :background
+          ,(plist-get my/base16-colors :base00)))))
+   `(tab-bar-tab-inactive
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base03)
+          :background
+          ,(plist-get my/base16-colors :base01)))))
+   `(eglot-highlight-symbol-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base06)
+          :weight
+          bold))))
+   `(region
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base01)
+          :background
+          ,(plist-get my/base16-colors :base04)))))
+   `(font-lock-keyword-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :yellow)))))
+   `(font-lock-variable-name-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base05)))))
+   `(font-lock-function-name-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :blue)))))
+   `(font-lock-function-call-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :cyan)))))
+   `(font-lock-builtin-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :blue)))))
+   `(font-lock-constant-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :orange)
+          :weight normal))))
+   `(font-lock-type-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :orange)
+          :weight normal))))
+   `(font-lock-string-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :green)
+          :weight normal))))
+   `(font-lock-operator-face
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base05)))))
+   `(org-level-1
+     ((t (:foreground
+          ,(plist-get my/base16-colors :violet)))))
+   `(org-level-2
+     ((t (:foreground
+          ,(plist-get my/base16-colors :blue)))))
+   `(org-level-3
+     ((t (:foreground
+          ,(plist-get my/base16-colors :cyan)))))
+   `(org-level-4
+     ((t (:foreground
+          ,(plist-get my/base16-colors :green)))))
+   `(org-level-5
+     ((t (:foreground
+          ,(plist-get my/base16-colors :red)))))
+   `(org-level-6
+     ((t (:foreground
+          ,(plist-get my/base16-colors :orange)))))
+   `(org-level-7
+     ((t (:foreground
+          ,(plist-get my/base16-colors :yellow)))))
+   `(org-level-8
+     ((t (:foreground
+          ,(plist-get my/base16-colors :brown)))))
+   `(org-agenda-done
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base03)))))
+   `(org-agenda-current-time
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base06)))))
+   `(org-time-grid
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base04)))))
+   `(org-headline-done
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base03)))))
+   `(org-drawer
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base03)))))
+   `(org-verbatim
+     ((t (:foreground
+          ,(plist-get my/base16-colors :green)))))
+   `(org-link
+     ((t (:foreground
+          ,(plist-get my/base16-colors :cyan)
+          :underline t))))
+   `(denote-faces-date
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base03)))))
+   `(doom-modeline-project-dir
+     ((t (:foreground
+          ,(plist-get my/base16-colors :cyan)
+          :slant
+          italic))))
+   `(doom-modeline-buffer-file
+     ((t (:foreground
+          ,(plist-get my/base16-colors :base07)
+          :slant
+          italic
+          :weight
+          bold))))
+   )
+)
 
 ;; Default face (gui only)
 (unless (not (display-graphic-p))
