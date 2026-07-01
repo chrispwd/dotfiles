af3deca84a0984d8e2d71352f26f315d02b921de
chrispwd <chrispwd@protonmail.com>
2026-07-01 01:20:38 -0600

    fix(emacs): c-or-c++-ts-mode on header files

--- emacs/.emacs.d/modules/default-config.el
+++ emacs/.emacs.d/modules/default-config.el
@@ -997,7 +997,8 @@ and restart Flymake to apply the changes."
 ;;; CPP-TS-MODE
 (use-package c-ts-mode
   :ensure nil
-  :mode "\\.\\(c\\|h\\)\\'"
+  :mode "\\.c\\'"
+  :mode "\\.h\\'" 
   :defer 't
   :config
   (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
@@ -1013,6 +1014,14 @@ and restart Flymake to apply the changes."
   (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
   (add-to-list 'treesit-language-source-alist
              '(cpp "https://github.com/tree-sitter/tree-sitter-cpp" "master" "src")))
+
+;;; C-OR-CPP-TS-MODE
+(use-package c-or-c++-ts-mode
+  :ensure nil
+  :mode "\\.h\\'"
+  :defer 't
+  :config
+  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode)))
 
 ;;; ------------------- EMACS-SOLO CUSTOMS
 ;;; EMACS-SOLO-HOOKS
